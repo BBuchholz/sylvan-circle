09685ba353f00d780f96a70ac0decedaeb1b8578
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-22 15:29:01 -0500

    Add intro to SYLFAN

---
+++ src/pages/myrKiSs/SYLFAN.vue
@@ -0,0 +1,39 @@
+<template>
+  <p class="display">
+    COMING SOON: Sylvan Circle Academy, a place for Dabblers and Dilletantes to become Learned Participants in the spirit of that very Dabbling and Dilletantery that gets such a bad wrap in the press...
+  </p>
+  <p class="display">
+    Don't let another tell you how (Y)OUR Learning Path should be navigated, gather all resources available and make sensible choices on what's best for (Y)OUR OWN GROWTH.
+  </p>
+  <p class="display">
+    Click the card below to request more information
+  </p>
+  <div>
+    <a
+      href="https://forms.gle/iN5su4m5twnrKv819"
+      target="_blank"
+    >
+      <img class="card" src="https://madamadam.s3.us-east-2.amazonaws.com/SYLFAN-857a_CARD.png">
+    </a>
+  </div>
+</template>
+
+<style>
+  .display {
+    margin: 40px;
+  }
+
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
