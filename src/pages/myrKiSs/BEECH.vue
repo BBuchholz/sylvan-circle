afe8e0096cda5bedfff6f9ce39ca5ee5cb7548ce
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-21 23:14:55 -0500

    Add BEECH

---
+++ src/pages/myrKiSs/BEECH.vue
@@ -0,0 +1,39 @@
+<template>
+  <p class="display">
+    BEECH (Fagus spp.)
+  </p>
+  <div>
+    <!-- <a href="#" target="_blank"> -->
+    <img class="card" src="https://madamadam.s3.us-east-2.amazonaws.com/BEECH-9060_CARD.png">
+    <!-- </a> -->
+  </div>
+  <p class="display">
+    Card Photo Source:
+    <a
+      href="https://www.inaturalist.org/photos/133547858"
+      target="_blank"
+    >
+      iNaturalist Photo 133547858, (c) psweet, some rights reserved (CC BY-SA), uploaded to iNaturalist by psweet
+    </a>
+  </p>
+</template>
+
+  <style>
+    .display {
+      margin: 40px;
+    }
+
+    .card {
+      max-width: 50%;
+      height: auto;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      border: 5px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 1%;
+    }
+  </style>
---
+++ src/pages/myrKiSs/TMPLT.vue
@@ -0,0 +1,33 @@
+<template>
+  <p class="display">
+    info here
+  </p>
+  <div>
+    <!-- <a href="#" target="_blank"> -->
+    <!-- <img class="card" src=""> -->
+    <!-- </a> -->
+  </div>
+  <p class="display">
+    card attribution info here
+  </p>
+</template>
+
+  <style>
+    .display {
+      margin: 40px;
+    }
+
+    .card {
+      max-width: 50%;
+      height: auto;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      border: 5px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 1%;
+    }
+  </style>
