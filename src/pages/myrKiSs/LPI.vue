5779b43b75c6263f5cbe034b6d0e6aeeb945050c
Brent Buchholz <brentbuchholz@gmail.com>
2023-10-02 08:08:53 -0500

    Add LPI

---
+++ src/pages/myrKiSs/LPI.vue
@@ -0,0 +1,39 @@
+<template>
+  <p class="display">
+    Part of our Learning Paths here at SCA are Investigatory, that is to say, we encourage Learning Participants to find their own conclusions given the materials at hand. To this end, we prefer to collect sources for the sake of sharing and encourage discussion of what is covered in those sources.
+  </p>
+  <p class="display">
+    We do not necessarily ascribe to all the views held in the Source materials chosen, it is a useful exercise to the Learning Participant to get used to holding multiple views at one time, weighing their benefits and drawbacks without ruling out possibilities for alternative views unless they are inherently contradictory to core integrity.
+  </p>
+  <p class="display">
+    Find the truths that are true for you, but also be open to the truths of others and willing to explore evolutions and mutations as they arise. This is tha Learned Path ov Fervent Ability Intelligently Thriving, tha LP ov FAITH that reaches for the Apple (NB: tha etymology ov Apple...)
+  </p>
+  <div>
+    <!-- <a href="#" target="_blank"> -->
+    <img class="card" src="https://madamadam.s3.us-east-2.amazonaws.com/LPI-60fc_CARD.png">
+    <!-- </a> -->
+  </div>
+  <p class="display">
+    card attribution info here
+  </p>
+</template>
+
+  <style>
+    .display {
+      margin: 40px;
+    }
+
+    .card {
+      max-width: 50%;
+      height: auto;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      border: 5px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 1%;
+    }
+  </style>
